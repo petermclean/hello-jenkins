89d7526fdf5759c2dbf2f0544a4a01c253c68c2f
peter mclean <peter.mclean@sage.com>
2015-03-15 16:44:48 +0000

    Switch to hello jenkins1234

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins123');
+  res.send('hello jenkins1234');
 });
  
 app.listen(process.env.PORT || 5000);
